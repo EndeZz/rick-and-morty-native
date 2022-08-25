987e6206083c583f9dae3f3d670f948ceab1c88b
endezz <r-serega13@yandex.ru>
2022-08-25 11:16:07 +0400

    fix: episode icon on active mode

--- src/ui/Icon/EpisodeIcon/index.tsx
+++ src/ui/Icon/EpisodeIcon/index.tsx
@@ -11,6 +11,7 @@ export const EpisodeIcon: FC<EpisodeIconProps> = ({ focused }) => (
     <Path
       d="M23.9991 8.16666H5.33248C4.04381 8.16666 2.99915 9.21133 2.99915 10.5V23.3333C2.99915 24.622 4.04381 25.6667 5.33248 25.6667H23.9991C25.2878 25.6667 26.3325 24.622 26.3325 23.3333V10.5C26.3325 9.21133 25.2878 8.16666 23.9991 8.16666Z"
       stroke={focused ? theme.color.primary : theme.color.icon_default}
+      fill={focused ? theme.color.primary : 'transparent'}
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
